45d28cf97b3809812c56378405426d07e6ccaedf
hungvo-Original <this.is.my.other.mail.123@gmail.com>
2021-11-19 15:39:38 +0700

    load moving object data :dart:

--- CS202_Project/MovingObject.h
+++ CS202_Project/MovingObject.h
@@ -1,13 +1,47 @@
-#ifndef _MOVING_OBJECT_H
-#define _MOVING_OBJECT_H
+#ifndef _MOVING_OBJECT_H_
+#define _MOVING_OBJECT_H_
+
+#include<iostream>
+#include<fstream>
+#include"SFML/Graphics.hpp"
+
+using namespace sf;
+using namespace std;
+
+const string DATA_PATH = "Data/";
+const string MOVING_OBJECT_TYPE[] = { "Player", "Bird", "Dinosaur", "Car", "Truck" };
 
 class MovingObject {
 private:
 	float speed;
+	RectangleShape model;
+	bool moveToLeft;
+
+	void LoadData(string type) {
+		string path = DATA_PATH + type + ".txt";
+
+		ifstream in(path);
+		if (in.is_open()) {
+			in >> speed;
+
+			int width, height;
+			in >> width >> height;
+			model.setSize(Vector2f(width, height));
+		}
+
+		in.close();
+	}
 
 public:
-	float getSpeed() {
-		return speed;
+	MovingObject(string type, Vector2f position, bool moveToLeft) {
+		LoadData(type);
+		model.setPosition(position);
+		this->moveToLeft = moveToLeft;
+	}
+
+	virtual void move() {
+		Vector2f moveDirection(moveToLeft ? 1 : -1, 0);
+		model.move(moveDirection * speed);
 	}
 };
-#endif // !_MOVING_OBJECT_H
+#endif // !_MOVING_OBJECT_H_
