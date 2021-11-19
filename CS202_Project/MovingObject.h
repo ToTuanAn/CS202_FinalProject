e54ca8287cc6a3f2086e5d608aa8fc1541d4013d
hungvo-Original <this.is.my.other.mail.123@gmail.com>
2021-11-19 11:54:54 +0700

    it's time to learn sfml :book:

---
+++ CS202_Project/MovingObject.h
@@ -0,0 +1,13 @@
+#ifndef _MOVING_OBJECT_H
+#define _MOVING_OBJECT_H
+
+class MovingObject {
+private:
+	float speed;
+
+public:
+	float getSpeed() {
+		return speed;
+	}
+};
+#endif // !_MOVING_OBJECT_H
