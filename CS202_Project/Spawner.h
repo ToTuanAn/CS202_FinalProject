a123ebcaf8ba364fb6d4ee69d88a008e6c97e884
vohuynh19 <vohuynh01092002@gmail.com>
2021-11-25 15:23:34 +0700

    Create spawner!!

---
+++ CS202_Project/Spawner.h
@@ -0,0 +1,31 @@
+#ifndef SPAWNER_H
+#define SPAWNER_H
+#include "Enemy.h"
+#include "GameWorld.h"
+#include "MovingObject.h"
+#include "SFML/Graphics.hpp"
+#include "fstream"
+#include "string"
+#include <iostream>
+#include <vector>
+
+using namespace sf;
+using namespace std;
+class Spawner
+{
+private:
+    float speed, timeFromLastSwitchAnim = 0, newObjectTime;
+    Vector2f position;
+    string type;
+    vector<MovingObject *> listEnemy;
+
+public:
+    Spawner(float newObjectTime, Vector2f position, string type)
+    {
+        this->newObjectTime = newObjectTime;
+        this->position = position;
+        this->type = type;
+    }
+};
+
+#endif
